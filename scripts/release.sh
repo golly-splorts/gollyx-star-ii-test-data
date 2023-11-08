ebfd9590efbe0485fa74068288d60ba918c7d32e
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2023-11-08 09:39:54 -0800

    fix typo

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,8 @@ set -x
 
 REMOTE="ghx"
 
-if [ -z "${GOLLYX_STAR_DATA_HOME}" ]; then
-	echo 'You must set the $GOLLYX_STAR_DATA_HOME environment variable to proceed.'
+if [ -z "${GOLLYX_STAR_II_DATA_HOME}" ]; then
+	echo 'You must set the $GOLLYX_STAR_II_DATA_HOME environment variable to proceed.'
 	exit 1
 fi
 
